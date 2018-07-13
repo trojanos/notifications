fd81f2c84d8c521283ff05cfe0ec7a0c391959ab
trojanos <trojanos09@gmail.com>
2018-07-13 20:59:43 +0300

    ui

---
+++ app/src/main/java/test/notfs/troja/notificationstest/FragmentOne.java
@@ -0,0 +1,16 @@
+package test.notfs.troja.notificationstest;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentOne extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_one, null);
+    }
+}
--- app/src/main/java/test/notfs/troja/notificationstest/MainActivity.java
+++ app/src/main/java/test/notfs/troja/notificationstest/MainActivity.java
@@ -1,9 +1,16 @@
 package test.notfs.troja.notificationstest;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    FragmentManager fm;
+    FragmentTransaction ft;
+
+    FragmentOne fr1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
