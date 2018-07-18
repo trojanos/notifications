cee7125ed8d4b3facab1727212fb7fc6c2cf9487
trojanos <trojanos09@gmail.com>
2018-07-18 20:02:39 +0300

    initial

--- app/src/main/java/test/notfs/troja/notificationstest/FragmentOne.java
+++ app/src/main/java/test/notfs/troja/notificationstest/FragmentOne.java
@@ -6,11 +6,31 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FragmentOne extends Fragment {
+    Button createNewNotf;
+
     @Nullable
     @Override
+
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_one, null);
+
+        View v = inflater.inflate(R.layout.fragment_one, container, false);
+        initView(v);
+        return  v;
+    }
+
+    private void initView(View v) {
+        createNewNotf = (Button) v.findViewById(R.id.button_Create_notification);
+
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "1";
     }
 }
--- app/src/main/java/test/notfs/troja/notificationstest/MainActivity.java
+++ app/src/main/java/test/notfs/troja/notificationstest/MainActivity.java
@@ -1,20 +1,104 @@
 package test.notfs.troja.notificationstest;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 
-public class MainActivity extends AppCompatActivity {
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+
+public class MainActivity extends FragmentActivity {
+
+    static final String TAG = "myLogs";
     FragmentManager fm;
-    FragmentTransaction ft;
+//    FragmentTransaction ft;
+    TextView count;
 
-    FragmentOne fr1;
+    ViewPager pager;
+    PagerAdapter pagerAdapter;
+
+
+    ImageButton plusButton,minusButton;
+
+    static final int PAGE_COUNT = 10;
+    FragmentOne fr1 = new FragmentOne();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        plusButton = findViewById(R.id.button_plus);
+        minusButton = findViewById(R.id.button_minus);
+        count = findViewById(R.id.count);
+
+        pager = findViewById(R.id.pager);
+        pager.setAdapter(pagerAdapter);
+        pagerAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager());
+        pager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageSelected(int position) {
+                Log.d(TAG, "onPageSelected, position = " + position);
+            }
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset,
+                                       int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+
+        plusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                ft = getFragmentManager().beginTransaction();
+//                ft.replace(R.id.fragments, fr1);
+//                ft.addToBackStack("FragmentOne");
+//                ft.commit();
+            }
+        });
+
+        minusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+
     }
+    private class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+
+
+        public MyFragmentPagerAdapter(android.support.v4.app.FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return PageFragment.newInstance(position);
+        }
+
+        @Override
+        public int getCount() {
+            return PAGE_COUNT;
+        }
+        public CharSequence getPageTitle(int position) {
+            System.out.println(position);
+            return "Title " + position;
+
+        }
+
+    }
+
 }
